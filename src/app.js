d4540b83204d76f68614efe8db95f3fe747b4ab8
LagAntunes <lag151617@gmail.com>
2022-03-17 17:03:44 -0300

    adicionando métodos POST, GET e DELETE

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import {openDb} from './configDB.js';
-import {createTable, InsertPessoa} from './controler/pessoa.js'
+import {createTable, InsertPessoa, updatePessoa, selectPessoas, selectUnicaPessoa, deletePessoa} from './controler/pessoa.js'
 
 import express from 'express';
 const app = express();
@@ -11,6 +11,16 @@ app.get('/', function(req, res) {
     res.send('olá mundo')
 })
 
+app.get('/pessoas', async function(req, res) {
+    let pessoas = await selectPessoas();
+    res.json(pessoas);
+})
+
+app.get('/pessoa', async function(req, res) {
+    let pessoa = await selectUnicaPessoa(req.body.id);
+    res.json(pessoa);
+})
+
 app.post('/pessoa', function(req, res) {
     console.log(req.body)
     InsertPessoa(req.body)
@@ -18,4 +28,25 @@ app.post('/pessoa', function(req, res) {
         "statusCode": 200
     })
 })
+
+app.put('/pessoa', function(req, res) {
+    if(req.body && !req.body.id) {
+        res.json({
+            "statusCode": "400",
+            "msg": "Você precisa informar um Id"
+        })
+    } else {
+        console.log(req.body)
+        updatePessoa(req.body)
+        res.json({
+        "statusCode": 200
+    })
+    }
+})
+
+app.delete('/pessoa', async function(req, res) {
+    let pessoa = await deletePessoa(req.body.id);
+    res.json(pessoa);
+})
+
 app.listen(3000, () => console.log('API rodando'))
--- src/controler/pessoa.js
+++ src/controler/pessoa.js
@@ -11,3 +11,30 @@ export async function InsertPessoa(pessoa) {
         db.run('INSERT INTO pessoa (nome, idade) VALUES (?, ?)' , [pessoa.nome, pessoa.idade]);
     })
 }
+
+export async function updatePessoa(pessoa) {
+    openDb().then(db => {
+        db.run('UPDATE pessoa SET nome=? , idade=? WHERE id=?' , [pessoa.nome, pessoa.idade, pessoa.id]);
+    })
+}
+
+export async function selectPessoas() {
+    return openDb().then(db => {
+        return db.all('SELECT * FROM pessoa')
+        .then(res => res)
+    });
+}
+
+export async function selectUnicaPessoa(id) {
+    return openDb().then(db => {
+        return db.get('SELECT * FROM pessoa WHERE id=?', [id])
+        .then(res => res)
+    });
+}
+
+export async function deletePessoa(id) {
+    return openDb().then(db => {
+        return db.get('DELETE FROM pessoa WHERE id=?', [id])
+        .then(res => res)
+    });
+}
